133ab7866e601c0e4fe3457e0b586ca3b8629167
Yan Zhang <yanzhanggc@gmail.com>
2025-04-10 10:13:50 +0800

    fix os path (#5)

--- src/embedding_model/test_E5_embedder.py
+++ src/embedding_model/test_E5_embedder.py
@@ -1,14 +1,15 @@
-
-import torch
 import os
+import torch
 from transformers import AutoTokenizer, AutoModel
 
 task_names = ['EMP', 'mouse', 'promcore', 'prom300', 'splice', 'tf', 'virus']
 
-description_path = r"C:\Users\yz285\Desktop\Research_Projects\GeneLLM\src\dataset\description_V2"
+# Use os.path.join for cross-platform compatibility
+description_path = os.path.join(".", "dataset", "description_V2")
 descriptions = []
 for task in task_names:
-    with open(fr'{description_path}\{task}.txt', 'r') as f:
+    task_file_path = os.path.join(description_path, f"{task}.txt")
+    with open(task_file_path, 'r') as f:
         descriptions.append(f.read().replace('\n', ''))
         f.close()
 
@@ -21,15 +22,15 @@ batch_dict = tokenizer(descriptions, max_length=512, padding=True, truncation=Tr
 outputs = model(**batch_dict)
 embeddings = outputs.last_hidden_state
 
-# create embedding dic embeddings_named 
+# Create embedding dictionary
 embeddings_named = {name:  # name of the dataset
                     {'embedding_seq': embeddings[i],   # embedding seq with paddings
                      'attention_mask': batch_dict['attention_mask'][i],  # attention_mask for this embedding seq 
-                     'pooler_output': outputs.pooler_output[i]}  # pooler_output for this embedding seq(CLS embedding)  
+                     'pooler_output': outputs.pooler_output[i]}  # pooler_output for this embedding seq (CLS embedding)  
                     for i, name in enumerate(task_names)}
 
-
-base_folder = r"C:\Users\yz285\Desktop\Research_Projects\GeneLLM\src\data\GUE\processed"
+# Use os.path.join for base folder
+base_folder = os.path.join(".", "data", "GUE", "processed")
 
 for task in task_names:
     embedding_file_path = os.path.join(base_folder, task, "embedding_seq.pth")
@@ -39,7 +40,4 @@ for task in task_names:
     torch.save(embeddings_named[task]['attention_mask'], attention_mask_file_path)
     torch.save(embeddings_named[task]['pooler_output'], pooler_output_file_path)
 
-
-# save embeddings_named
-# torch.save(embeddings_named, r"C:\Users\yz285\Desktop\Research_Projects\GeneLLM\src\data\GUE\raw")
-
+print('finish! embeddings are saved in the data/GUE/processed folder')
